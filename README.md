ab9fadae412537043a979a6dda1d2b193462096c
Howl0 <101541614+Howl0@users.noreply.github.com>
2023-01-24 12:25:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CV-FM
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-gsv2fl)
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'This print is an example of string interpolation using one-way data binding ' + VERSION.major;
+}
